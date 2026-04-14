fde717d0b55db5dada9f6e04f2dcaa9b962305c2
Lary Peixoto <laryssa.peixot@outlook.com>
2026-04-14 19:32:39 -0300

    fix: troca de imagens dos parques.

--- mvp-tere-verde/src/components/cards/parkCard/ParkCard.jsx
+++ mvp-tere-verde/src/components/cards/parkCard/ParkCard.jsx
@@ -3,12 +3,17 @@ import { Card } from "../card/Card";
 import { Button } from "../../button/Button";
 
 export function ParkCard({ park }) {
+  const image =
+    park.fotos_urls && park.fotos_urls.length > 0
+      ? park.fotos_urls[0]
+      : "/placeholder.jpg";
+
   return (
     <Card className={styles.card}>
       <div className={styles.wrapper}>
         <div
           className={styles.image}
-          style={{ backgroundImage: `url(${park.fotos_urls?.[0]})` }}
+          style={{ backgroundImage: `url(${image})` }}
         />
       </div>
       <div className={styles.info}>
