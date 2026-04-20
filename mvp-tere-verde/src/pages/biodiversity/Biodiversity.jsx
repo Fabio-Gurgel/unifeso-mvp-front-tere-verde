a8e79a1666984778e24f22c9f49122712c1bf3ea
Lary Peixoto <laryssa.peixot@outlook.com>
2026-04-20 18:34:41 -0300

    feat: adiciona botões de filtro de fauna e flora na página de biodiversidade.

--- mvp-tere-verde/src/pages/biodiversity/Biodiversity.jsx
+++ mvp-tere-verde/src/pages/biodiversity/Biodiversity.jsx
@@ -1,12 +1,44 @@
 import styles from "./Biodiversity.module.css";
 
+import { useState } from "react";
+
 import { PageHeader } from "../../components/page-header/PageHeader";
 import { Button } from "../../components/button/Button";
 
 export function Biodiversity() {
+  const [selected, setSelected] = useState("fauna");
+
   return (
     <>
+      <PageHeader
+        title="Parques de Teresópolis"
+        subtitle="Descubra parques naturais para explorar a beleza da Serra dos Órgãos."
+      />
+      <main className={styles.container}>
+        <section className={styles.filterButtons}>
+          <Button
+            shape="pill"
+            type="button"
+            className={`${styles.filterBiodiversity} ${
+              selected === "fauna" ? styles.active : ""
+            }`}
+            onClick={() => setSelected("fauna")}
+          >
+            Fauna
+          </Button>
 
+          <Button
+            shape="pill"
+            type="button"
+            className={`${styles.filterBiodiversity} ${
+              selected === "flora" ? styles.active : ""
+            }`}
+            onClick={() => setSelected("flora")}
+          >
+            Flora
+          </Button>
+        </section>
+      </main>
     </>
   );
 }
