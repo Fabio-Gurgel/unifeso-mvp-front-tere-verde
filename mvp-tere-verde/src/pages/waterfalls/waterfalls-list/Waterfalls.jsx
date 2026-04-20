d6f55b26f0fefebdec6bc6bac63931b5d4527437
hugoPereir4 <hp971221@gmail.com>
2026-04-20 18:37:45 -0300

    feat: Adiciona a estrutura da página de cachoeiras e sua rota

---
+++ mvp-tere-verde/src/pages/waterfalls/waterfalls-list/Waterfalls.jsx
@@ -0,0 +1,14 @@
+//import styles from './Waterfalls.module.scss';
+
+import { PageHeader } from '../../../components/page-header/PageHeader';
+
+export function Waterfalls() {
+    return (
+        <>
+            <PageHeader
+                title="Cachoeiras de Teresópolis"
+                subtitle="Descubra as belas cachoeiras e poços naturais dos parques da região."
+            />
+        </>
+    )
+}
