a4b04cea6cb3c014eb08db1aef14ba06d867f584
Fabio-Gurgel <fabio.cc.gurgel@gmail.com>
2026-04-22 19:00:19 -0300

    🛡️ - Init adm: Criando header, layout e rota

--- mvp-tere-verde/src/routes/Router.jsx
+++ mvp-tere-verde/src/routes/Router.jsx
@@ -1,8 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 
 import { MainLayout } from "../layouts/main-layout/MainLayout";
+import { AdminLayout } from "../layouts/admin-layout/AdminLayout";
 import { Home } from "../pages/home/Home";
 import { Login } from "../pages/admin/login/login";
+import { DashboardAdm } from "../pages/admin/dashboard/Dashboard";
 import { Parks } from "../pages/parks/Parks";
 import { Trails } from "../pages/trails/Trails";
 import { Biodiversity } from "../pages/biodiversity/Biodiversity";
@@ -16,6 +18,9 @@ export function Router() {
         <Route path="/parques" element={<Parks />} />
         <Route path="/trilhas" element={<Trails />} />
         <Route path="/biodiversidade" element={<Biodiversity />} />
+      </Route>
+      <Route element={<AdminLayout />}>
+        <Route path="/admin/dashboard" element={<DashboardAdm />} />
       </Route>
     </Routes>
   );
